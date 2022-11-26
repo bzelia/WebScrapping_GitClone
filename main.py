22af99f79970729cfd81763d8d321cb6afdc3f5d
kassiazama <kassiazama@gmail.com>
2022-11-26 09:44:51 -0300

    os.chdir para baixar arquivo

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ if __name__ == '__main__':
     for x in range(len(repositorio)):
         print(str(x) + ' = ' + repositorio[x].text)
 
-    projeto = int(input('Numero do repositório para o clone: '))
+
+
+    projeto = int(input('Número do repositório para o clone: '))
     caminho = repositorio[projeto].text
 
     url = url + caminho
     soup = BeautifulSoup(requests.get(url).text, 'html5lib')
 
     nome_pasta = 'pasta_de_' + caminho
+    os.chdir('c://')
     os.system('git clone ' + url)
